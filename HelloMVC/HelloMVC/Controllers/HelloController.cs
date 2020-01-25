908ded1c68e8f2a72c23befd8715ffa2d29cbf74
Ramya <ramya4792@gmail.com>
2020-01-25 15:02:19 -0600

    studio3

--- HelloMVC/HelloMVC/Controllers/HelloController.cs
+++ HelloMVC/HelloMVC/Controllers/HelloController.cs
@@ -3,30 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace HelloMVC.Controllers
 {
-    public class HelloController : Controller
+    public  class HelloController : Controller
     {
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
-            string html = "<form method='post'>"+
+           
+            string html = "<form method='post'>" +
                 "<input type='text' name='name'/>" +
-                "<input type='submit' value='Greet'/>"+
+                "<select name='lang'>" +
+                "<option value='en' selected>English</option>" +
+                "<option value='sp'>Spanish</option>" +
+                "<option value='fr'>French</option>" +
+                "<option value='it'>Italian</option>" +
+                "<option value='ge'>German</option>" +
+                "</select>" +
+                "<input type='submit' value='GreetMe'/>" +
                 "</form>";
             return Content(html, "text/html");
-           // return Redirect("/Hello/Goodbye");
+        }
+        public static String CreateMessage(string name, string lang)
+        {
+            string  message= "";
+
+            if (lang == "sp")
+                message = "Hola ";
+            else if (lang == "ge")
+                message = "Hallo ";
+            else if (lang == "fr")
+                message = "Bonjour ";
+            else if (lang == "it")
+                message = "Ciao ";
+            else
+                message = "Hello ";
+            
+
+
+            return message+name;
         }
         // /hello
         [Route("/hello")]
         [HttpPost]
-        public IActionResult Display(string name="world")
+        public IActionResult Display(string name="world", string lang="en")
         {
-            return Content(string.Format("<h1>Hello {0}!</h1>", name), "text/html");
+            string hi=CreateMessage(name, lang);
+            return Content(string.Format("<h1>{0} !</h1>", hi), "text/html");
         }
         //handle requests to /hello/Name(URL segment)
         [Route("/Hello/{name}")]
